d433754759fcff168d72029e9abf3cfb74ac23ee
Drake Wheeler <109574097+Copypaster3000@users.noreply.github.com>
2024-09-17 19:28:50 -0700

    Update errors.py

--- errors.py
+++ errors.py
@@ -1,10 +1,7 @@
 #errors.py
 #Drake Wheeler
-#CS302
-#Program 4-5
-#Karla Fant
 #8/13/2024
-#This file holds the the custom error classes that are all derived from the built-in Exception class. This allows me to raise errors with custom variable names.
+#This file holds the custom error classes that are all derived from the built-in Exception class. This allows me to raise errors with custom variable names.
 
 class MissingDataError(Exception):
     #This Error is for when there is missing data
