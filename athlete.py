5443b1865cbe7a9e3809fe73623ba9e1cf01c660
Drake Wheeler <109574097+Copypaster3000@users.noreply.github.com>
2024-09-17 19:29:04 -0700

    Update athlete.py

--- athlete.py
+++ athlete.py
@@ -1,8 +1,5 @@
 #athlete.py
 #Drake Wheeler
-#CS302
-#Program 4-5
-#Karla Fant
 #8/13/2024
 #This file contains the athlete class, representing a runner in the race. The class stores the athlete's name, speed, and injury status. 
 #It provides methods to manage and retrieve these attributes, ensuring that the athlete's data is accurately maintained throughout the race.
